36c936468e1846108e997549ee8d1ecd2c0a8923
Fanynwork <2396039429@qq.com>
2024-02-07 10:53:24 +0800

    添加帖子属性

--- note_user_info.py
+++ note_user_info.py
@@ -16,11 +16,15 @@ def get_note_info(note_string, note_dic={}):
     note_dic["note_time"] = re.findall(r'"time":(\d+)', note_string)[0]
     note_dic["note_title"] = re.findall('"title":"(.*?)"', note_string)[0]
     note_dic["note_desc"] = re.findall('"desc":"(.*?)"', note_string)[0]
-
+    note_dic["ipLocation"] = re.findall('"ipLocation":"(.*?)"', note_string)[0]
+    note_dic["likeCount"] = re.findall('"likedCount":"(.*?)"', note_string)[0]
+    note_dic["collectCount"] = re.findall('"collectedCount":"(.*?)"', note_string)[0]
+    note_dic["commentCount"] = re.findall('"commentCount":"(.*?)"', note_string)[0]
+    note_dic["shareCount"] = re.findall('"shareCount":"(.*?)"', note_string)[0]
     return note_dic
 
 
-def get_user_info(user_string, user_dic):
+def get_user_info(user_string, user_dic={}):
     """
     提取帖子发布者
     Parameters
@@ -32,8 +36,7 @@ def get_user_info(user_string, user_dic):
     -------
     user_dic
     """
-    user_dic["user_id"] = re.findall('"userId":"(.*?)"', user_string)[0]
+    user_dic["userId"] = re.findall('"userId":"(.*?)"', user_string)[0]
     user_dic["nickname"] = re.findall('"nickname":"(.*?)"', user_string)[0]
-    user_dic["ipLocation"] = re.findall('"ipLocation":"(.*?)"', user_string)[0]
 
     return user_dic
