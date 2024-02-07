84ac63d38191d898683ab618cd24deb69361c324
Fanynwork <2396039429@qq.com>
2024-02-07 10:52:36 +0800

    修改

--- id_list_cursor.py
+++ id_list_cursor.py
@@ -15,11 +15,12 @@ def get_id_and_cursor_value(html):
     json_data = json.loads(html)
     # 获取游标值
     cursor = json_data["data"]["cursor"]
-
-    list_data = []
+    has_more = json_data["data"]["has_more"]
+    list_url = []
     # 获取该html中的6个帖子id值
-    for note_id in json_data["data"]["notes"]:
-        dic_data = {"note_id": note_id, "url": "https://www.xiaohongshu.com/explore/%s" % note_id }
-        list_data.append(dic_data)
+    for note in json_data["data"]["notes"]:
+        note_id = note["id"]
+        url = "https://www.xiaohongshu.com/explore/%s" % note_id
+        list_url.append(url)
 
-    return list_data, cursor
+    return list_url, cursor, has_more
